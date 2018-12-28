059562ad6c281bfa0b21563c9fc4a154bf4fff1b
Yoav Keren <yoavke@gmail.com>
2018-12-28 16:41:12 +0200

    fixed links bug crashes app

--- app/src/main/java/com/hit/gymapp/MainActivity.java
+++ app/src/main/java/com/hit/gymapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.hit.gymapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -27,5 +28,15 @@ public class MainActivity extends AppCompatActivity {
         //creating instance of DatabaseHelper to control the Database
         myDb = new DatabaseHelper(this);
 
+    }
+
+    private class MyBrowser extends WebViewClient
+    {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url)
+        {
+            view.loadUrl(url);
+            return true;
+        }
     }
 }
