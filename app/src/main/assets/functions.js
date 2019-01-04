26b7b75243f8538b54b24f3a117f25678ac15eb2
Yoav Keren <yoavke@gmail.com>
2019-01-04 14:21:20 +0200

    aesthetics in details page

--- app/src/main/assets/functions.js
+++ app/src/main/assets/functions.js
@@ -85,7 +85,7 @@ function retrieve() {
         activities = json.activities;
         let date = new Date(parseInt(activities[0].timestamp));
         document.querySelector("#details").innerHTML += "<h2>" +activities[0].activity+ " (" + date.getDate() +"/"+ (parseInt(date.getMonth())+1) +"/"+ date.getFullYear() + ")</h2>";
-        document.querySelector("#details").innerHTML += "<p>"+activities[0].length + " " + (activities[0].activity=='swimming'?"pools":"km") + "edit</a> | delete</p>";
+        document.querySelector("#details").innerHTML += "<p>"+activities[0].length + " " + (activities[0].activity=='swimming'?"pools":"km") + " <a href=''>edit</a> | <a href=''>delete</a></p>";
     }
     else if(page=='addAerobic.html') {
 
